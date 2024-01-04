c856d2cf1222871467675dc3bd74279cfb2d96b4
Christian Anderson <tehninjawaffle@gmail.com>
2024-01-04 15:03:08 -0800

    Included 3 new columns, seems to be working fine after running it

--- data/migrations/20240104225115_first-migration.js
+++ data/migrations/20240104225115_first-migration.js
@@ -5,6 +5,9 @@
 exports.up = async function(knex) {
   await knex.schema.createTable("project", table => {
     table.increments("project_id").notNullable()
+    table.string("project_name", 100).notNullable()
+    table.string("project_description")
+    table.boolean("project_completed").defaultTo(false)
   })
   await knex.schema.createTable("resource", table => {
     table.increments("resource_id").notNullable()
