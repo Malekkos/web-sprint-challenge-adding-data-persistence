857983aa5a86e64b6a97baac6eeb27be84ed3dc9
Christian Anderson <tehninjawaffle@gmail.com>
2024-01-04 15:45:26 -0800

    Added a conditonal and forEach to make it so that the return true or false is actually returned as true or false, instead of 0 or 1

--- api/project/model.js
+++ api/project/model.js
@@ -4,6 +4,15 @@ const db = require("../../data/dbConfig")
 
 async function getAll() {
   const data = await db("project")
+  console.log(data)
+  data.forEach(field => {
+    console.log(field.project_completed)
+    if(field.project_completed === 0) {
+      field.project_completed = false
+    } else {
+      field.project_completed = true
+    }
+  })
   return data
 }
 
