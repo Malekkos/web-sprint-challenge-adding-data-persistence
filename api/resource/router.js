e2651ba5a437419af8ec78a112cc5419ef5712a0
Christian Anderson <tehninjawaffle@gmail.com>
2024-01-04 14:43:00 -0800

    Setup server.js and the 3 routers to be able to properly hit endpoints

--- api/resource/router.js
+++ api/resource/router.js
@@ -1,1 +1,11 @@
 // build your `/api/resources` router here
+
+const router = require("express").Router()
+const Resource = require("./model")
+
+router.get("*", (req, res, next) => {
+  res.json({ message: "Inside of the resource Router!"})
+})
+
+
+module.exports = router
--- api/task/router.js
+++ api/task/router.js
@@ -1,1 +1,11 @@
 // build your `/api/tasks` router here
+
+const router = require("express").Router()
+const Task = require("./model")
+
+router.get("*", (req, res, next) => {
+  res.json({ message: "Inside of the task Router!"})
+})
+
+
+module.exports = router
